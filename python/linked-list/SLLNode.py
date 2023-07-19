4702b17153c5da83ba5bb504fa5914ed34f4ecd5
Waled Khalid <wak327@gmail.com>
2023-07-20 02:10:10 +0500

    Problem 3,4,5

--- python/linked-list/SLLNode.py
+++ python/linked-list/SLLNode.py
@@ -2,3 +2,23 @@ class LinkedListNode:
     def __init__(self, value):
         self.value = value
         self.next = None
+    
+    def str(self):
+        current_node = self
+        string = '['
+        while current_node:
+            if current_node.next is None:
+                string += str(current_node.value)
+                break
+            string += str(current_node.value) + ', '
+            current_node = current_node.next
+        string += ']'
+        return string
+    
+    def to_list(self):
+        current_node = self
+        list = []
+        while current_node:
+            list.append(current_node.value)
+            current_node = current_node.next
+        return list
